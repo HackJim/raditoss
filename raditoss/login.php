e73abec8764339ba40bd2405fac8feb50b836616
HackJim <Jim_Feng@cn.dlink.com>
2014-03-08 21:15:33 +0800

    version1.0: Create the pages

---
+++ raditoss/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>用户登录</title>
+<meta name="description"
+	content="敢做不怕二，镭星人是一个鼓励工程爱好者进行创意工程项目的社区，在里面可以找到创意的项目，认识优秀的工程人员。">
+<meta name="keywords" content="工程,创意,工程项目,创意工程项目,镭星人,工程师,优秀工程师">
+<meta name="author" content="HackJim">
+<link rel="shortcut icon" href="images/ico/favicon.png">
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/css/bootstrap.min.css">
+
+<!-- Custom styles for this template -->
+<link href="css/login.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+	<div class="container">
+
+		<form class="form-signin" role="form" action="index.php">
+			<h2 class="form-signin-heading">请您登录</h2>
+			<input type="text" class="form-control" placeholder="电子信箱" required autofocus>
+			<input type="password" class="form-control" placeholder="用户密码" required>
+			<label class="checkbox"><input type="checkbox" value="remember-me"> 记住密码 </label>
+			<button class="btn btn-lg btn-primary btn-block" type="submit">登录</button>
+		</form>
+
+	</div>
+</body>
+</html>
--- raditoss/parter.php
+++ raditoss/parter.php
@@ -2,7 +2,7 @@
 <html lang="zh-CN">
 <head>
 <meta charset="utf-8">
-<title>镭星人</title>
+<title>合作伙伴</title>
 <meta name="description"
 	content="敢做不怕二，镭星人是一个鼓励工程爱好者进行创意工程项目的社区，在里面可以找到创意的项目，认识优秀的工程人员。">
 <meta name="keywords" content="工程,创意,工程项目,创意工程项目,镭星人,工程师,优秀工程师">
@@ -13,32 +13,7 @@
 <link rel="shortcut icon" href="images/ico/favicon.png">
 </head>
 <body>
-<!-- 	<header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner"> -->
-<!-- 		<div class="container"> -->
-<!-- 			<div class="navbar-header"> -->
-<!-- 				<button class="navbar-toggle" type="button" data-toggle="collapse" -->
-<!-- 					data-target=".bs-navbar-collapse"> -->
-<!-- 					<span class="sr-only">Toggle navigation</span> <span -->
-<!-- 						class="icon-bar"></span> <span class="icon-bar"></span> <span -->
-<!-- 						class="icon-bar"></span> -->
-<!-- 				</button> -->
-<!-- 				<a href="index.php" class="navbar-brand"><img alt="镭星人" src="images/ico/favicon.png" height="26" width="26">&nbsp;镭星人</a> -->
-<!-- 			</div> -->
-<!-- 			<nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation"> -->
-<!-- 				<ul class="nav navbar-nav"> -->
-<!-- 					<li><a href="post.php">发布项目</a></li> -->
-<!-- 					<li><a href="parter.php">合作伙伴</a></li> -->
-<!-- 					<li><a href="about.php">关于组织</a></li> -->
-<!-- 					<li><a href="offline.php">线下活动</a></li> -->
-<!-- 				</ul> -->
-<!-- 				<ul class="nav navbar-nav navbar-right"> -->
-<!-- 					<li><a href="login.php">登录</a></li> -->
-<!-- 					<li><a href="register.php">注册</a></li> -->
-<!-- 				</ul> -->
-<!-- 			</nav> -->
-<!-- 		</div> -->
-<!-- 	</header> -->
-	
+
 	<!-- Fixed navbar -->
     <div class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="container">
@@ -53,9 +28,9 @@
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="index.php">主页</a></li>
+            <li><a href="index.php">主页</a></li>
             <li><a href="post.php">发布项目</a></li>
-			<li><a href="parter.php">合作伙伴</a></li>
+			<li class="active"><a href="parter.php">合作伙伴</a></li>
 			<li><a href="about.php">关于组织</a></li>
 			<li><a href="offline.php">线下活动</a></li>
           </ul>
@@ -72,4 +47,5 @@
 		src="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/js/bootstrap.min.js"></script>
 </body>
 </html>
+
 
